1c9f0c9de2d3c8a438edfc8d4aff54ee3143b6b9
Hayk <hayktunyan1994@gmail.com>
2024-05-10 10:37:27 +0400

    update Resume

--- components/hero.js
+++ components/hero.js
@@ -38,8 +38,7 @@ const Hero = () => {
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
               <a
-                href="./pdf/CV_Hayk_Tunyan.pdf"
-
+                href="./pdf/Hayk_Tunyan_Resume.pdf"
                 download
                 rel="noopener"
                 className="px-8 py-4 text-lg font-medium text-center text-white bg-indigo-600 rounded-md ">
